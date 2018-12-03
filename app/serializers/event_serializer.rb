b8ad75329db9feed0fb13fe104074da8a6c89046
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-12-03 09:11:02 -0500

    fixed event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :time, :description, :host_id, :address, :city, :state, :zipcode
+  attributes :id, :date, :name, :time, :description, :host_id, :address, :city, :state, :zipcode, :users, :host
   has_many :user_events
   has_many :users, through: :user_events
   belongs_to :host, {class_name: 'User'}
