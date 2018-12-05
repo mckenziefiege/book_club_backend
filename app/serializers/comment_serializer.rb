6a5a060d52921ad6ec7bed089eabd4c616c54fa9
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-12-05 16:47:52 -0500

    added readable date and created_at to comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :event_id, :content
+  attributes :id, :user_id, :event_id, :content, :created_at
   attribute :user do
     object.user
+  end
+  attribute :readable_time do
+    object.created_at.strftime("%m/%d/%Y at %I:%M%p")
   end
 end
